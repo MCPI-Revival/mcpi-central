325fb13dc911f443dbb6e0e3f667b1d0ecf97754
Gamer4life1 <lpuvilla0001@mymail.lausd.net>
2021-02-15 12:57:07 -0800

    Add new table

--- src/db.py
+++ src/db.py
@@ -32,6 +32,7 @@ class DBData:
 
 	def setup(self):
 		self.cur.execute("CREATE TABLE IF NOT EXISTS servers (name VARCHAR(64) NOT NULL, ip TEXT NOT NULL, port SMALLINT NOT NULL CHECK (port > 0), owner VARCHAR(64) NOT NULL, PRIMARY KEY (name));");
+		self.cur.execute("CREATE TABLE IF NOT EXISTS users (username VARCHAR(64) NOT NULL, email VARCHAR(64) NOT NULL, PRIMARY KEY (username));")
 		self.conn.commit();
 		return 0;
 
